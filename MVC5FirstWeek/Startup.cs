0033cebf5919c18c5c45209a7adccde4e941affa
labrus-huang <labrus.huang@gmail.com>
2015-07-06 12:13:13 +0800

    首次將專案加入Git，初步建立了「客戶資料」維護功能及首頁連結

---
+++ MVC5FirstWeek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5FirstWeek.Startup))]
+namespace MVC5FirstWeek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
